70d3f2a18ae588d24d013f41c648c6b540b389b4
Ramita20 <ramiro_larrea18@hotmail.com>
2024-07-20 02:15:36 -0300

    interacciones del textarea.

--- js/app.js
+++ js/app.js
@@ -1,2 +1,25 @@
+/* --------------------------- CÓDIGO DEL TEXTAREA ------------------------------*/
 const textarea = document.getElementById("texto-entrada");
-textarea.style.height = `${textarea.scrollHeight}px`;
+const placeHolder = "Ingrese el texto aquí";
+
+textarea.value = "Ingrese el texto aquí";
+
+// Redimenciona verticalmente el textarea a medida que se escribe en él.
+textarea.addEventListener("input", () => {
+  textarea.style.height = "auto";
+  textarea.style.height = `${textarea.scrollHeight}px`;
+});
+
+// Elimina el placeholder del campo si se selecciona para escribir.
+textarea.addEventListener("focus", () => {
+  if (textarea.value === placeHolder) {
+    textarea.value = "";
+  }
+});
+
+// Reescribe el placeholder en el campo, si el mismo esta vacío.
+textarea.addEventListener("blur", () => {
+  if (textarea.value.trim() === "") {
+    textarea.value = placeHolder;
+  }
+});
